7266cfa53823a9ed4d6605f5c0b55764885c8bb5
Marco Pivetta <ocramius@gmail.com>
2014-09-15 04:35:00 +0200

    Selecting first car

---
+++ example/run-select-car.php
@@ -0,0 +1,16 @@
+<?php
+
+use Example\Car;
+
+/* @var $entityManager \Doctrine\ORM\EntityManager */
+$entityManager = require __DIR__ . '/bootstrap.php';
+
+$car = $entityManager->find(Car::class, 1);
+
+var_dump('Car found:', $car);
+
+if (! $car) {
+    return;
+}
+
+var_dump('Users in Car:', $car->users->toArray());
