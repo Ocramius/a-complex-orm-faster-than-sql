476d63d693df00470bf8e78cefcf5fe8bad81e97
Marco Pivetta <ocramius@gmail.com>
2014-09-15 04:30:40 +0200

    FindAll example

---
+++ example/run-select-all.php
@@ -0,0 +1,10 @@
+<?php
+
+use Example\User;
+
+/* @var $entityManager \Doctrine\ORM\EntityManager */
+$entityManager = require __DIR__ . '/bootstrap.php';
+
+$users = $entityManager->getRepository(User::class)->findAll();
+
+var_dump('Users found:', $users);
